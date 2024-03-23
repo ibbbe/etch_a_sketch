d692ce1df8589edf1ca64d6d183746a6387c934f
ibbbe <130594784+ibbbe@users.noreply.github.com>
2024-03-23 17:01:04 +0100

    Add functions(createGrid, clearGrid), button clear
    In index.html created button 'Clear', that activates
     function clearGrid(gets user input, clear grid and
     calls function createGrid).
    
     Function createGrid creates new grid.
    
    On branch main
    
     Changes to be committed:
    	modified:   etch.js
    	modified:   index.html

--- etch.js
+++ etch.js
@@ -1,1 +1,26 @@
 const div = document.querySelector('#container');
+const clear = document.querySelector('#clear');
+
+clear.addEventListener('click', clearGrid);
+
+
+function createGrid(numOfSquares) {
+
+    for (let i = 0; i < numOfSquares * numOfSquares; i++) {
+        const square = document.createElement('div');
+        square.textContent = "Abba";
+        div.appendChild(square);
+        square.style.backgroundColor = "black";
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = "red";
+        });
+    }
+}
+
+function clearGrid() {
+    const numOfSquares = parseInt(prompt("Please input the number:"));
+    while (div.hasChildNodes()) {
+        div.removeChild(div.firstChild);
+    }
+    createGrid(numOfSquares);
+} 
