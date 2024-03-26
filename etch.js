f876a88a90f82f97b2a92bee0453b40500520815
ibbbe <130594784+ibbbe@users.noreply.github.com>
2024-03-26 15:13:54 +0100

    Add style.css, finish project
    In style.css done design for button clear, div
    container and grid square.
    In index.html renamed button id from clear to btn.
    In etch.js removed square.textContent, added class
    square to square. Defined square size(height and width)
    by dividing the container size with number of squares.
    
    Changes to be committed:
    	modified:   etch.js
    	modified:   index.html
    	modified:   style.css

--- etch.js
+++ etch.js
@@ -1,30 +1,29 @@
-const div = document.querySelector('#container');
-const clear = document.querySelector('#clear');
+const container = document.querySelector('#container');
+const clear = document.querySelector('#btn');
 
 clear.addEventListener('click', clearGrid);
 
 
 function createGrid(numOfSquares) {
-
     for (let i = 0; i < numOfSquares * numOfSquares; i++) {
         const square = document.createElement('div');
-        square.textContent = "Abba";
-        div.appendChild(square);
-        square.style.backgroundColor = "black";
-        square.addEventListener('mouseover', () => {
-            square.style.backgroundColor = "red";
-        });
+        square.classList.add('square');
+        const squareSize = 600 / numOfSquares;
+        container.appendChild(square);
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+        square.addEventListener('mouseover', () => { square.style.backgroundColor = "red"; });
     }
 }
 
 function clearGrid() {
     const numOfSquares = parseInt(prompt("Please input the number:"));
     if (numOfSquares > 100) {
-        alert('Please input the number between 1 and 100.')
+        alert('Please input the number between 1 and 100.');
         clearGrid();
     }
-    while (div.hasChildNodes()) {
-        div.removeChild(div.firstChild);
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
     }
     createGrid(numOfSquares);
 } 
